ab0cd63386930392d10be63a94c82dc0efb3422b
Mendhie <imendhie@yahoo.com>
2020-09-14 16:24:36 +0100

    Added Firebase Remote Config to alert for app update

--- app/src/main/java/com/solojet/dialup/MainActivity.java
+++ app/src/main/java/com/solojet/dialup/MainActivity.java
@@ -7,10 +7,24 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 import fragments.FavoriteFragment;
 import fragments.HistoryFragment;
@@ -28,9 +42,14 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     final Fragment fragmentFav = new FavoriteFragment();
     final Fragment fragmentHistory = new HistoryFragment();
 
-
     private Fragment fragmentActive = fragmentHome;
     private FragmentManager fragmentManager = getSupportFragmentManager();
+
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
+    private final long versionCode = BuildConfig.VERSION_CODE;
+    private final String FB_RC_KEY_FORCE_UPDATE_VERSION = "force_update_version";
+    private final String FB_RC_KEY_LATEST_VERSION = "latest_version";
+    private final HashMap<String, Object> defaultMap = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +82,9 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         }
         else
             loadFragment();
+
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        checkForUpdate();
     }
 
     private void loadFragment() {
@@ -104,4 +126,68 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         outState.putString(LAST_FRAGMENT, fragmentActive.getTag());
         super.onSaveInstanceState(outState);
     }
+    private void checkForUpdate() {
+        // Hashmap which contains the default values for all the parameter defined in the remote config server
+        defaultMap.put(FB_RC_KEY_FORCE_UPDATE_VERSION, versionCode);
+        defaultMap.put(FB_RC_KEY_LATEST_VERSION, versionCode);
+
+        long cacheExpiration = BuildConfig.DEBUG? 60 : TimeUnit.HOURS.toSeconds(12);
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setMinimumFetchIntervalInSeconds(cacheExpiration).build();
+
+        mFirebaseRemoteConfig.setConfigSettingsAsync(configSettings);
+        mFirebaseRemoteConfig.setDefaultsAsync(defaultMap);
+        mFirebaseRemoteConfig.fetchAndActivate().addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()) {
+
+                boolean updated = task.getResult();
+                Log.d("Move", "Config params updated: " + updated);
+
+                boolean visible = true;
+                long forceUpdateVersion = mFirebaseRemoteConfig.getLong(FB_RC_KEY_FORCE_UPDATE_VERSION);
+                long latestAppVersion = mFirebaseRemoteConfig.getLong(FB_RC_KEY_LATEST_VERSION);
+                Log.i("Move", "onComplete: version code: "+ versionCode + "latest code: " + latestAppVersion);
+                if (latestAppVersion > versionCode){
+                    if(forceUpdateVersion>versionCode)
+                        visible = false;
+                    updateAlert(visible);
+                }
+            }
+        });
+    }
+    private void updateAlert(boolean visible){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.update_alert, null);
+        builder.setView(dialogView).setCancelable(false);
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+        Button btnUpdate = alertDialog.findViewById(R.id.btnUpdate);
+        TextView btnLater = alertDialog.findViewById(R.id.btnLater);
+        btnLater.setVisibility(visible? View.VISIBLE : View.GONE);
+        btnUpdate.setOnClickListener(view -> rateApp());
+        btnLater.setOnClickListener(view -> alertDialog.cancel());
+    }
+
+    public void rateApp(){
+        Uri uri = Uri.parse("market://details?id=" + getApplicationContext().getPackageName());
+        Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+        // To count with Play market backstack, After pressing back button,
+        // to taken back to our application, we need to add following flags to intent.
+
+        goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
+                Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+
+        try {
+            startActivity(goToMarket);
+        } catch (ActivityNotFoundException e) {
+            startActivity(new Intent(Intent.ACTION_VIEW, uri));
+        }
+    }
+
+
+
+
 }
